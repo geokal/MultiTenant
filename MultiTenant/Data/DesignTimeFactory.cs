ed3de5e60952624239f0f4003f6b3bd787127550
George K <gkalpak@fogus.gr>
2024-01-31 00:16:46 +0200

    first commit

---
+++ MultiTenant/Data/DesignTimeFactory.cs
@@ -0,0 +1,16 @@
+using Finbuckle.MultiTenant;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace MultiTenant.Data;
+
+public class SharedDesignTimeFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var tenantInfo = new TenantInfo{ ConnectionString = "Data Source=Data/SharedIdentity.db" };
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+        return new ApplicationDbContext(tenantInfo, optionsBuilder.Options);
+    }
+}
